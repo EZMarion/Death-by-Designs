ad108af93a8c51e6cb857f90077a2105dc24d3ef
EZMarion <eintzuumaung@gmail.com>
2025-07-29 14:17:34 +0630

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDffoLenAKrKGMia63SDWTFt5E4AK3hxBE",
   authDomain: "novel-site-81674.firebaseapp.com",
@@ -13,5 +7,8 @@ const firebaseConfig = {
   appId: "1:227276404115:web:0a5831851c8c05391b6466"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+
+firebase.initializeApp(firebaseConfig);
+
+
+const auth = firebase.auth();
